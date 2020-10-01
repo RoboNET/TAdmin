a810ba5ab31cc1388f88c716b772c488e91df57a
Mikhail Korolev <michael443959@gmail.com>
2020-10-01 14:57:59 +0300

    Поменял версию .net

--- TAdmin.Web/Startup.cs
+++ TAdmin.Web/Startup.cs
@@ -1,3 +1,8 @@
+using System;
+using GraphQL;
+using GraphQL.DataLoader;
+using GraphQL.Server;
+using GraphQL.Types;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -5,8 +10,10 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using TAdmin.Controllers;
 using TAdmin.DataSource.Mssql;
+using TAdmin.GraphQL;
 
 namespace TAdmin
 {
@@ -27,11 +34,29 @@ namespace TAdmin
                 options.JsonSerializerOptions.Converters.Add(new TableConverter());
                 options.JsonSerializerOptions.Converters.Add(new FieldMetadataConverter());
             });
-            
-            services.AddSingleton<IDataSource, MssqlDataSource>();
+
+            services
+                .AddSingleton<AdminSchema>()
+                .AddGraphQL((options, provider) =>
+                {
+                    options.EnableMetrics = true;
+                    var logger = provider.GetRequiredService<ILogger<Startup>>();
+                    options.UnhandledExceptionDelegate = ctx =>
+                        logger.LogError("{Error} occured", ctx.OriginalException.Message);
+                })
+                // Add required services for de/serialization
+                .AddSystemTextJson(deserializerSettings => { }, serializerSettings => { }) // For .NET Core 3+
+                .AddErrorInfoProvider(opt => opt.ExposeExceptionStackTrace = true)
+                .AddDataLoader() // Add required services for DataLoader support
+                .AddGraphTypes(typeof(AdminSchema));
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
+            
+            services.AddHttpContextAccessor();
+
+            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +79,13 @@ namespace TAdmin
 
             app.UseRouting();
 
+            // add http for Schema at default url /graphql
+            app.UseGraphQL<ISchema>();
+
+            // use graphql-playground at default url /ui/playground
+            app.UseGraphQLPlayground();
+            
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
