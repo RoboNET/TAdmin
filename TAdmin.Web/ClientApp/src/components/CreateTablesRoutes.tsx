0e908da6e9452080361bf30243ead1dc1947e835
ildarzak <ildar.zak.v@gmail.com>
2020-10-05 21:25:53 +0300

    исправил конфликты и форматирование

--- TAdmin.Web/ClientApp/src/components/CreateTablesRoutes.tsx
+++ TAdmin.Web/ClientApp/src/components/CreateTablesRoutes.tsx
@@ -6,7 +6,9 @@ export const CreateTablesRoutes = ({ databases }: any) => {
     return (
         <Switch>
             {databases.map((database: any) =>
-                database.tables.map((table: any) => <Route path={`/${database.name}/${table.name}`} render={props => (<TableView {...props} table={table} />)} />))}
+                database.tables.map((table: any) =>
+                    <Route path={`/${database.name}/${table.name}`} render={props => (
+                        <TableView {...props} table={table} dbName={database.name} />)} />))}
         </Switch>
     )
 }
--- TAdmin.Web/ClientApp/src/components/Layout.js
+++ TAdmin.Web/ClientApp/src/components/Layout.js
@@ -4,11 +4,7 @@ import { NavMenu } from './NavMenu';
 
 export const Layout = ({children, databases}) => {
   return (
-<<<<<<< HEAD
-    <Container className={'base-container'}>
-=======
     <Container className={"base-margin"}>
->>>>>>> master
       <Row>
         <Col xs="3">
           <NavMenu databases={databases} />
